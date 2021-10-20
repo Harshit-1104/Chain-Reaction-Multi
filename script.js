ae9b437539631526e33fcad47ec893a2baa2b351
MiHarsh <harshm17172612@gmail.com>
2021-10-20 23:18:32 +0530

    improved backend

--- script.js
+++ script.js
@@ -67,12 +67,19 @@ io.of("/").on("connection", (socket) => {
       playerID: data.userID,
     });
 
+    // upgrade the matrix in server - sole truth #Game Server
+    updateGrid(data.userClick.X, data.userClick.Y, data.userID, data.roomId);
+
     socket.broadcast.to(data.roomId).emit("isTurn", {
       numberOfTurns: turn,
       userTurn: (turn % socket.adapter.rooms[data.roomId].length) + 1,
     });
 
     turn++;
+  });
+
+  socket.on("sync_mat", (data) => {
+    socket.emit("sync_mat", { gameMatrix: cache[data.roomId]["gameMatrix"] });
   });
 
   socket.on("disconnect", (data) => {
@@ -91,7 +98,7 @@ io.of("/").on("connection", (socket) => {
 });
 
 function initializeGrid(rows, columns) {
-  grid = new Array(rows + 2);
+  let grid = new Array(rows + 2);
   for (var i = 0; i < grid.length; i++) {
     grid[i] = new Array(columns + 2);
   }
@@ -114,3 +121,85 @@ function initializeGrid(rows, columns) {
 
   return grid;
 }
+
+function updateGrid(X, Y, userID, roomName) {
+  var queue = [];
+  queue.push([X, Y]);
+
+  while (queue.length !== 0) {
+    var curr = queue.shift();
+
+    if (
+      curr[0] < 1 ||
+      curr[0] > cache[roomName]["gameMatrix"].length - 2 ||
+      curr[1] < 1 ||
+      curr[1] > cache[roomName]["gameMatrix"].length - 2
+    )
+      continue;
+
+    var idx = getIdx(curr[0], curr[1], roomName);
+    let lim = detLim(curr[0], curr[1], roomName);
+
+    if (cache[roomName]["gameMatrix"][curr[0]][curr[1]][0] < lim) {
+      cache[roomName]["gameMatrix"][curr[0]][curr[1]][0] += 1;
+
+      cache[roomName]["gameMatrix"][curr[0]][curr[1]][1] = userID;
+    } else {
+      cache[roomName]["gameMatrix"][curr[0]][curr[1]][0] = 0;
+      cache[roomName]["gameMatrix"][curr[0]][curr[1]][1] = 0;
+
+      queue.push([curr[0] + 1, curr[1]]);
+      queue.push([curr[0] - 1, curr[1]]);
+      queue.push([curr[0], curr[1] + 1]);
+      queue.push([curr[0], curr[1] - 1]);
+    }
+  }
+}
+
+function getIdx(X, Y, roomName) {
+  return (cache[roomName]["gameMatrix"].length - 2) * (X - 1) + Y - 1;
+}
+
+function detLim(X, Y, roomName) {
+  var len = cache[roomName]["gameMatrix"].length;
+
+  if (X > 1 && X < len - 2 && Y > 1 && Y < len - 2) return 3;
+  else if (
+    [X, Y].equals([1, 1]) ||
+    [X, Y].equals([1, len - 2]) ||
+    [X, Y].equals([len - 2, 1]) ||
+    [X, Y].equals([len - 2, len - 2])
+  )
+    return 1;
+  else return 2;
+}
+
+// Custom method to compare arrays +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Warn if overriding existing method
+if (Array.prototype.equals)
+  console.warn(
+    "Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code."
+  );
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = function (array) {
+  // if the other array is a falsy value, return
+  if (!array) return false;
+
+  // compare lengths - can save a lot of time
+  if (this.length != array.length) return false;
+
+  for (var i = 0, l = this.length; i < l; i++) {
+    // Check if we have nested arrays
+    if (this[i] instanceof Array && array[i] instanceof Array) {
+      // recurse into the nested arrays
+      if (!this[i].equals(array[i])) return false;
+    } else if (this[i] != array[i]) {
+      // Warning - two different object instances will never be equal: {x:20} != {x:20}
+      return false;
+    }
+  }
+  return true;
+};
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
